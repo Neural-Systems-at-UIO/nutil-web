3b10e006c6e697cac944a005a4ffc43fd8180dae
leuat <leuat@irio.co.uk>
2020-11-20 12:34:03 +0100

    Changed paths

--- project/settings.py
+++ project/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_PATH = BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -126,10 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR + "/static",
-    '/var/www/static/',
-]
+STATICFILES_DIRS = (PROJECT_PATH + "/static/",)
 
+STATIC_ROOT = (PROJECT_PATH)
